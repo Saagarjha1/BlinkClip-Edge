58942a893bbe8eaa072038b1e0c79c2499b639ce
Saagarjha1 <126740871+Saagarjha1@users.noreply.github.com>
2025-08-02 13:56:59 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/Fullsatck/Signup_login/frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer className="bg-gray-800 text-white p-4 text-center">
-    &copy; {new Date().getFullYear()} MyApp. All rights reserved.
-  </footer>
-);
-
-export default Footer;
